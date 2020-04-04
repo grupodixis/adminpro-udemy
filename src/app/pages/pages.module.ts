8d9ef3db55e549ef89c9b252f6f8d7ee73ac0c68
unknown <pablo@DXSPR003.dixis.es>
2020-04-04 18:36:47 +0200

    Input, Ouput y ViewChild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,24 +5,33 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import {FormsModule} from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
+/* TEMPORAL */
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
-    declarations:[
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
+    ],
+    exports: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
         Graficas1Component
     ],
-    exports:[
-        PagesComponent,
-        DashboardComponent,
-        ProgressComponent,
-        Graficas1Component
-    ],
-    imports:[
+    imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
     ]
 })
 export class PagesModule { }
